c072982b33d617ff848dab611fb3ba6c4e239efa
saroj923 <sarojkafle5@gmail.com>
2023-04-29 20:40:55 +1000

    basic setup for web api

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet] // api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await context.Activities.FindAsync(id);
+        }
+
+    }
+}
